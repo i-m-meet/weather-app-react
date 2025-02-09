0c37cef235e4bcb1e6a89ba3f4ca53a8453c9cc4
Sumeet Sumeet <sumeet.bx1@gmail.com>
2025-02-08 22:35:40 -0500

    card UI complete

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -7,6 +7,25 @@ const Weather = () => {
         <input type="text" placeholder="search" className="h-12 rounded-3xl text-[18px] border-none outline-none pl-6 bg-[#ebfffc] text-[#626262]" />
         <img src={assets.search_icon} alt="" className="w-12.5 p-4 rounded-3xl bg-[#ebfffc] cursor-pointer" />
       </div>
+      <img src={assets.clear_icon} alt="" className="w-[150px] mt-[30px] mb-[30px]" />
+      <p className="text-7xl/tight text-white">16°C</p>
+      <p className="text-white text-4xl">Location</p>
+      <div className="w-full mt-10 text-white flex justify-between">
+        <div className="flex items-center gap-3 text-2xl">
+            <img src={assets.humidity_icon} alt="" className="w-7  "/>
+            <div className="flex flex-col items-start">
+                <p>91 %</p>
+                <span className="block text-[16px]">Humidity</span>
+            </div>
+        </div>
+        <div className="flex items-start gap-3 text-2xl">
+            <img src={assets.wind_icon} alt="" className="w-7 mt-2.5 "/>
+            <div className="flex flex-col items-start">
+                <p>3.6 Km/h</p>
+                <span className="block text-[16px]">Wind Speed</span>
+            </div>
+        </div>
+      </div>
     </div>
   );
 };
